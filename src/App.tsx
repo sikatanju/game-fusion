3069ee53aecca697ad82253311c5a3cd16c1d26c
sikatanju <saadtanju@gmail.com>
2024-10-27 11:06:31 +0600

    Build platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show, VStack } from "@chakra-ui/react";
 
 import NavBar from "./components/NavBar.tsx";
 import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenreList.tsx";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres.ts";
+import PlatformSelector from "./components/PlatformSelector.tsx";
 
 function App() {
     const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -34,6 +35,7 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area="main">
+                <PlatformSelector />
                 <GameGrid selectedGenre={selectedGenre}></GameGrid>
             </GridItem>
         </Grid>
